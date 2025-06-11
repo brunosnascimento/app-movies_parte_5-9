4e54e21bebc8153236be0715928ec9fd3bd3ace3
brunosnascimento <matbsn@gmail.com>
2025-06-10 21:48:58 -0300

    Aula 10/06/2025

---
+++ src/components/Category/index.js
@@ -0,0 +1,13 @@
+import styles from "./Category.module.css"
+
+function Category({ category, children }) {
+    return (
+            <section className={styles.category}>
+                <h2> {category}</h2>
+                <div>
+                    { children }
+                </div>
+            </section>
+    );
+}
+export default Category;
